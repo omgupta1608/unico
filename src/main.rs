906a88b063cd202970681bb3bfb6d3946701d2f0
Om Gupta <omgupta1608@gmail.com>
2023-07-19 21:05:15 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#![allow(dead_code)]
+
+use std::io;
+
+enum ConversionType {
+    LENGTH,
+    MASS,
+    // ANGLE,
+    // AREA,
+    // VOLUME,
+}
+
+// enum 
+
+fn convert(_val: f32) -> f32 {
+    return 1.1
+}
+
+fn converter_factory(conversion_type: ConversionType) -> impl Fn(f32) -> f32 {
+    match conversion_type {
+        ConversionType::LENGTH => print!("Length"),
+        ConversionType::MASS => print!("mass")
+    }
+    return convert;
+}
+
+
+fn main() -> io::Result<()>{
+    // let pattern: String = std::env::args().nth(1).expect("no pattern given");
+    // print!("{}", pattern)
+    let mut a = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut a);
+    print!("{}", a);
+    Ok(())
+}
