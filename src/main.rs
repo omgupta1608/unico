d31b2ca17fc32cb588404524843c1dc8ac9579d1
Om Gupta <omgupta1608@gmail.com>
2023-07-20 21:49:33 +0530

    added exits and converter picking

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use std::io;
 use std::collections::HashMap;
+use std::process;
 
 enum ConversionType {
     LENGTH,
@@ -44,6 +45,11 @@ fn converter_factory(conversion_type: &str) -> impl Fn(f32) -> f32 {
     return convert_length;
 }
 
+fn ExitOnError(msg: &str) {
+    println!("Error: {}", msg);
+    process::exit(1)
+}
+
 fn main() -> io::Result<()> {
     let value: String = std::env::args().nth(1).expect("No value provided");
     let f_val: f32 = value.as_str().parse::<f32>().unwrap();
@@ -56,27 +62,33 @@ fn main() -> io::Result<()> {
     for i in 0..conversion_types.len() {
         match conversion_types.get(i) {
             Some(val) => println!("{}. {}", i + 1, val),
-            None => (),
+            None => ExitOnError(""),
         }
     }
+    
     let mut c_choice = String::new();
     let stdin = io::stdin();
     let _ = stdin.read_line(&mut c_choice);
 
-    print!("{}", c_choice);
     let mut i_c_choice = -1; 
     match c_choice.trim().parse::<i32>() {
         Ok(val) => i_c_choice = val,
-        Err(..) => println!("Error: Invalid choice")
+        Err(..) => ExitOnError("Invalid Choice"),
     }
 
     if i_c_choice > conversion_types.len() as i32 {
-        panic!("Invalid Choice")
+        ExitOnError("Invalid Choice")
     }
 
-    // let c_type: &str
+    let mut c_conv_f: &str = "";
+    match index_to_type.get(&i_c_choice) {
+        Some(v) => c_conv_f = v,
+        None => ()
+    }
     
-    print!("Converting {}", c_choice);
+    print!("Converting {}", c_conv_f);
+    converter_factory(c_conv_f)(f_val);
+    
 
     Ok(())
 }
